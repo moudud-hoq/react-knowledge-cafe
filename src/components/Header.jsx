6ca92466e579efdbf3dafbe68c240a4af26c4b09
moududulhaque <moududulhaque@gmail.com>
2024-03-12 19:24:17 +0600

    Few Design Added

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="HeaderMainDiv flex justify-between px-16">
+      <h1 className="text-2xl font-bold text-center p-4">Knowledge Cafe</h1>
+      <h1 className="text-2xl font-bold text-center p-4">Knowledge Cafe</h1>
+    </div>
+  );
+};
+
+export default Header;
